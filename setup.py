4fbf8214c5831b0dcfd940ea41b52415fe9615b6
Gunnlaugur Þór Briem <gunnlaugur@gmail.com>
2015-03-22 13:44:25 +0000

    DataMarket patch version 10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='django-authority',
-    version='0.5-datamarket9',
+    version='0.5-datamarket10',
     description=(
         "A Django app that provides generic per-object-permissions "
         "for Django's auth app."
